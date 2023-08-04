3416551fe99eb7fbc521540569761beeb1792dc6
Trada Kevin <tradakevin0099@gmail.com>
2023-08-05 01:55:07 +0530

    change

---
+++ src/assets/index.js
@@ -0,0 +1,96 @@
+import logo from "./logo.svg";
+import tklogo2 from "./tklogo2.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import blender from "./tech/Blender.png";
+import cp from "./tech/C.png";
+import csharp from "./tech/CSharp.png";
+import cpp from "./tech/C++.png";
+import figma from "./tech/figma.png";
+import krita from "./tech/Krita.png";
+import photoshop from "./tech/Photoshop.png";
+import python from "./tech/Python.png";
+import trello from "./tech/Trello.png";
+import unity from "./tech/Unity2.png";
+import unreal from "./tech/Unreal.png";
+import visualStudio from "./tech/VisualStudio.png";
+import vscode from "./tech/VSCode.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import galanto from "./company/Galanto.png"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import cosmosgame from "./cosmos_combat.png"
+import Rollerballgame from "./Rollerball_Rampage.png"
+import Katanagame from "./Katana_Rush.png"
+
+export {
+  logo,
+  tklogo2,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  blender,
+  cp,
+  csharp,
+  cpp,
+  figma,
+  krita,
+  photoshop,
+  python,
+  trello,
+  unity,
+  unreal,
+  visualStudio,
+  vscode,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  galanto,
+  carrent,
+  jobit,
+  tripguide,
+  cosmosgame,
+  Rollerballgame,
+  Katanagame
+};
