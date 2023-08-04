d4ba33268627353bdd183d9b65acbbad60293e7b
Trada Kevin <tradakevin0099@gmail.com>
2023-08-05 02:50:54 +0530

    My Website Lunch
    happy :)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "https://enjoy0099.github.io/MyPortfoliof/",
+  base: "https://enjoy0099.github.io/MyPortfolio/",
   plugins: [react()],
 })
 
