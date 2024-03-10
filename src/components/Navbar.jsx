767894223d07c5d4aceef75d419a0df37220cb11
Trada Kevin <tradakevin0099@gmail.com>
2024-03-10 19:22:43 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         >
           <img src={tklogo2} alt="logo" className="w-9 h-9 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            <span className="sm:block hidden">Trada Kevin</span>
+            <span className="sm:block hidden">Trada</span>
           </p>
         </Link>
         <ul className=" list-none hidden sm:flex flex-row gap-10">
